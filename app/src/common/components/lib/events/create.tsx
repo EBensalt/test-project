1852347c6b67523bd3e9884442ef4d7ec5d6ba63
ebensalt <ebensalt@student.1337.ma>
2025-03-10 05:14:16 +0000

    event creation

---
+++ app/src/common/components/lib/events/create.tsx
@@ -0,0 +1,96 @@
+import useAuth from "@modules/auth/hooks/api/useAuth";
+import { Box, Button, Card, Dialog, TextField } from "@mui/material";
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import axios from "axios";
+import dayjs, { Dayjs } from "dayjs";
+import { useState } from "react";
+
+export default function Create() {
+	const [open, setOpen] = useState(false);
+  	const [data, setData] = useState({ title: "", date: dayjs(), location: "", max_participants: 1, description: ""});
+  	const { user } = useAuth();
+
+	function openDialog() {
+		setOpen(true);
+	}
+	function closeDialog() {
+		setOpen(false);
+	}
+	function changeText(event: React.ChangeEvent<HTMLInputElement>) {
+	setData({...data, [event.target.name]: event.target.value});
+	}
+	function chageDate(event: Dayjs | null) {
+	setData({...data, date: event ?? dayjs()});
+	}
+	async function addEvent() {
+	let mp = data.max_participants
+	if (typeof mp == "string")
+	  mp = 1;
+	try {
+	  const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/events`, { 
+		...data,
+		max_participants: 1,
+		user_id: user?.id
+	  });
+
+	  closeDialog();
+	} catch (e) {
+	  if (axios.isAxiosError(e))
+		console.log(e.response?.data);
+	}
+	}
+
+	return <>
+			<Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: 2 }}>
+    		  <Button variant="outlined" onClick={openDialog}>
+    		    create an event
+    		  </Button>
+    		</Box>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<Dialog open={open} maxWidth="xs" fullWidth>
+				<Card sx={{ padding: "10px", display: 'flex', flexDirection: "column", gap: "10px", width: "100%" }} >
+				<TextField
+					id="outlined-basic"
+					label="Title"
+					name='title'
+					variant="outlined"
+					value={data.title}
+					onChange={changeText}
+				/>
+				<DatePicker label="Date" value={data.date} onChange={chageDate} />
+				<TextField
+					name='location'
+					id="outlined-basic"
+					label="Location"
+					variant="outlined"
+					value={data.location}
+					onChange={changeText}
+				/>
+				<TextField
+					name='max_participants'
+					id="outlined-basic"
+					label="Maximum number of participants"
+					variant="outlined"
+					type="number"
+					value={data.max_participants}
+					onChange={changeText}
+				/>
+				<TextField
+					name='description'
+					id="outlined-basic"
+					label="Description"
+					variant="outlined"
+					multiline
+					value={data.description}
+					onChange={changeText}
+				/>
+				<Box sx={{ display: 'flex', justifyContent: "center", gap: "10px" }}>
+					<Button variant="outlined" onClick={addEvent}>Save</Button>
+					<Button variant="outlined" color='error' onClick={closeDialog}>Cancel</Button>
+				</Box>
+				</Card>
+			</Dialog>
+			</LocalizationProvider>
+	</>
+}
